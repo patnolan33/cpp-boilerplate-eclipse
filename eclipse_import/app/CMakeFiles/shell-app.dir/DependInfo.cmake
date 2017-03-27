6814ff0b52f95b3432754acb7312320742795f94
Patrick Nolan <patnolan33@gmail.com>
2017-03-27 16:59:54 -0500

    Directory eclipse_import added for future imports into the Eclipse IDE. This directory will include .project and .cproject files, as well as other CMake files that allow for easy import, build, and run with Eclipse.

---
+++ eclipse_import/app/CMakeFiles/shell-app.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/viki/Documents/ENPM808X/Week6/ExtraCredit/eclipse/cpp-boilerplate-eclipse/app/AnalogSensor.cpp" "/home/viki/Documents/ENPM808X/Week6/ExtraCredit/eclipse/cpp-boilerplate-eclipse/eclipse_import/app/CMakeFiles/shell-app.dir/AnalogSensor.cpp.o"
+  "/home/viki/Documents/ENPM808X/Week6/ExtraCredit/eclipse/cpp-boilerplate-eclipse/app/main.cpp" "/home/viki/Documents/ENPM808X/Week6/ExtraCredit/eclipse/cpp-boilerplate-eclipse/eclipse_import/app/CMakeFiles/shell-app.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ eclipse_import/test/CMakeFiles/cpp-test.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/viki/Documents/ENPM808X/Week6/ExtraCredit/eclipse/cpp-boilerplate-eclipse/test/main.cpp" "/home/viki/Documents/ENPM808X/Week6/ExtraCredit/eclipse/cpp-boilerplate-eclipse/eclipse_import/test/CMakeFiles/cpp-test.dir/main.cpp.o"
+  "/home/viki/Documents/ENPM808X/Week6/ExtraCredit/eclipse/cpp-boilerplate-eclipse/test/test.cpp" "/home/viki/Documents/ENPM808X/Week6/ExtraCredit/eclipse/cpp-boilerplate-eclipse/eclipse_import/test/CMakeFiles/cpp-test.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/viki/Documents/ENPM808X/Week6/ExtraCredit/eclipse/cpp-boilerplate-eclipse/eclipse_import/vendor/googletest/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../test/../vendor/googletest/googletest/include"
+  "../vendor/googletest/googletest/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
