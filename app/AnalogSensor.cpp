c06505617a176eccf9feafb8736c1739aea351f9
Patrick Nolan <patnolan33@gmail.com>
2017-03-27 17:33:22 -0500

    Adding Google Style guide XML file to import into Eclipse. Also ran formatter on the source files and test files so that they adhere to Google Style guides.

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -4,20 +4,18 @@
 #include <memory>
 
 AnalogSensor::AnalogSensor(unsigned int samples)
-    : mSamples(samples)
-{
+    : mSamples(samples) {
 }
 
-AnalogSensor::~AnalogSensor()
-{
+AnalogSensor::~AnalogSensor() {
 }
 
-int AnalogSensor::Read()
-{
-	std::shared_ptr<std::vector<int> > readings = std::make_shared<std::vector<int> >(mSamples, 10);
+int AnalogSensor::Read() {
+  std::shared_ptr<std::vector<int> > readings = std::make_shared<
+      std::vector<int> >(mSamples, 10);
 
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
-    return result;
+  double result = std::accumulate(readings->begin(), readings->end(), 0.0)
+      / readings->size();
+  return result;
 }
-
 
